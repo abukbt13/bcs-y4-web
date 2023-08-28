67d23f7e4da92b2c99ddfa9e1aafcb24e36cf496
Freemano <freemano@MACs-MacBook-Pro.local>
2023-08-28 22:19:38 +0300

    beginning

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Header from "@/views/includes/Header.vue";
+</script>
+
+<template>
+  <Header />
+  <i class="bi bi-2-circle-fill"></i>
+</template>
---
+++ src/views/includes/Header.vue
@@ -0,0 +1,31 @@
+
+<script setup>
+
+</script>
+
+<template>
+  <nav style="background-color: rgb(13, 250, 5)" class="navbar navbar-expand-lg ">
+    <div class="container-fluid">
+      <a class="navbar-brand text-primary" href="/">BCS YEAR FOUR</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<style scoped>
+
+</style>
