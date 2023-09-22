26ebe483bebe51bad6b1f7dd611aff64d15c5af0
Freemano <freemano@MACs-MacBook-Pro.local>
2023-09-23 00:00:15 +0300

    final

--- src/compossables/auth.js
+++ src/compossables/auth.js
@@ -4,16 +4,17 @@ import axios from "axios";
 export function auth(){
     const authHeader = { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } };
     const profile= ref([])
-    // const base_url = ref('https://ben.kwetunyumbani.online/api/')
-    // const storage = ref('https://ben.kwetunyumbani.online/backend/public/profiles/')
+    const base_url = ref('https://bcsy4.kwetunyumbani.online/api/')
+    const storage = ref('https://bcsy4.kwetunyumbani.online/backend/public/')
+    const storage_profile = ref('https://bcsy4.kwetunyumbani.online/backend/public/profiles/')
 
-    const base_url = ref('http://127.0.0.1:8000/api/')
-    const storage = ref('http://127.0.0.1:8000/')
-    const storage_profile = ref('http://127.0.0.1:8000/profiles/')
+    // const base_url = ref('http://127.0.0.1:8000/api/')
+    // const storage = ref('http://127.0.0.1:8000/')
+    // const storage_profile = ref('http://127.0.0.1:8000/profiles/')
 
     const   authUser = async () => {
         try {
-            const res= await axios.get('http://127.0.0.1:8000/api/auth/user', authHeader);
+            const res= await axios.get(base_url.value+'auth/user', authHeader);
 
         } catch (error) {
             window.location.href = '/login';
