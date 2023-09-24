818f0e04c2b66496729adb788d30a9390e92b691
Freemano <freemano@MACs-MacBook-Pro.local>
2023-09-24 10:42:37 +0300

    change

--- src/views/Group/Group.vue
+++ src/views/Group/Group.vue
@@ -2,9 +2,17 @@
 <script setup>
 import Header from "@/views/includes/Header.vue";
 import Footer from "@/views/includes/Footer.vue";
-import {group} from "@/compossables/group";
-import {onMounted} from "vue";
-const {create_group,group_name,group_description,group_category,showGroup,groups} = group()
+import {onMounted, ref} from "vue";
+
+import {auth} from "@/compossables/auth";
+const {base_url,storage_notes,authHeader}=auth()
+import axios from "axios";
+const groups = ref([])
+
+const showGroup = async () =>{
+  const res = await axios.get(base_url.value+'group/show')
+  groups.value =res.data.group
+}
 onMounted(() => {
   showGroup()
 })
@@ -12,7 +20,7 @@ onMounted(() => {
 
 <template>
 <Header />
-
+{{groups}}
       <router-link v-for="group in groups" :key="group" :to="{ path: `/show_group/posts`, query: { name: group.group_id } }" class="text-decoration-none btn btn-primary m-1 text-uppercase">
         {{ group.group_name }}
       </router-link>
